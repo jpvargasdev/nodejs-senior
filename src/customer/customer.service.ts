aff78b5cb24af574c4795527f793b31686d39e30
jp-vargas <vargasm.jp@gmail.com>
2023-08-24 10:13:41 +0200

    CRUD operations for customers (get, update, delete) by id or email;

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { GetCustomerInput } from './dto/customer.input';
+import { Customer, Prisma } from '@prisma/client';
 
 @Injectable()
 export class CustomerService {
@@ -13,6 +14,35 @@ export class CustomerService {
       take,
       cursor,
       where,
+    });
+  }
+
+  async create(params: Prisma.CustomerCreateInput) {
+    return this.prisma.customer.create({ data: params });
+  }
+
+  async get(id: string) {
+    return this.prisma.customer.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async remove(id: string) {
+    return this.prisma.customer.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: string, data: Partial<Customer>) {
+    return this.prisma.customer.update({
+      where: {
+        id,
+      },
+      data,
     });
   }
 }
